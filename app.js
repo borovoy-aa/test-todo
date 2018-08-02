d18e28a58f004fcfe84f343fed45c8c741426378
borovoy-aa <work.borovoy@gmail.com>
2018-08-02 21:50:19 +0300

    some change

--- app.js
+++ app.js
@@ -3,21 +3,57 @@ const newTask = document.getElementById('new-task');
 const mainWrap = document.getElementById('main');
 const listItem = document.getElementsByClassName('list');
 
+const clear = () => {
+	newTask.value = "";
+}
 
 const nTask = () => {
-    let taskValue = newTask.value;
-    let li = document.createElement('li');
-    li.classList.add('list')
-    li.innerHTML = taskValue;
-    mainWrap.appendChild(li);
+	const taskValue = newTask.value;
+	
+	const li = document.createElement('li');
+	li.classList.add('list')
+	
+
+	const removeBtn = document.createElement('button');
+	removeBtn.classList.add('removeBtn');
+	removeBtn.innerText = 'Remove Task';
+
+	var p = document.createElement('p');
+	p.classList.add('list-p');
+	li.appendChild(p);
+
+	p.innerHTML = taskValue;
+	li.appendChild(removeBtn);
+
+	if (taskValue.trim()) {
+		mainWrap.appendChild(li);
+	}
+
+	clear();
+
+	removeBtn.addEventListener('click', function(e) {
+		mainWrap.removeChild(li);
+	});
+
+	var editInput = document.createElement('input');
+	p.addEventListener('click', function() {
+		
+
+		var pVal = p.textContent;
+		var eInputVal = editInput.value = pVal;
+
+		li.removeChild(p);
+		li.insertBefore(editInput, removeBtn);
+
+		editInput.onblur = () => {
+			p.innerText = editInput.value;
+			li.insertBefore(p, removeBtn);
+			li.removeChild(editInput);
+			
+		}
+
+	})
 }
 
-const liTarget = e => {
-    let target = e.target;
-    console.log(target);
-    if (target.tagName != 'li') return;
-}
 
 btnTask.addEventListener('click', nTask, false);
-
-mainWrap.addEventListener('click', liTarget, false);
