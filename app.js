3f57fba6bbb3634a0aee20a458ffc3f72428a9d7
borovoy-aa <work.borovoy@gmail.com>
2018-08-03 13:32:30 +0300

    changed

--- app.js
+++ app.js
@@ -18,13 +18,22 @@ const nTask = () => {
 	removeBtn.classList.add('removeBtn');
 	removeBtn.innerText = 'Remove Task';
 
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('editBtn');
+	editBtn.innerText = 'Edit Task';
+
+	const okBtn = document.createElement('button');
+	okBtn.classList.add('okBtn');
+	okBtn.innerText = 'Okay';
+
 	var p = document.createElement('p');
 	p.classList.add('list-p');
 	li.appendChild(p);
 
 	p.innerHTML = taskValue;
 	li.appendChild(removeBtn);
-
+	li.appendChild(editBtn);
+	
 	if (taskValue.trim()) {
 		mainWrap.appendChild(li);
 	}
@@ -36,21 +45,25 @@ const nTask = () => {
 	});
 
 	var editInput = document.createElement('input');
-	p.addEventListener('click', function() {
+	
+	editBtn.addEventListener('click', function() {
 		
-
 		var pVal = p.textContent;
 		var eInputVal = editInput.value = pVal;
 
 		li.removeChild(p);
 		li.insertBefore(editInput, removeBtn);
+		editInput.autofocus = true;
+		li.removeChild(editBtn);
+		li.appendChild(okBtn);
 
-		editInput.onblur = () => {
+		okBtn.addEventListener('click', function() {
 			p.innerText = editInput.value;
 			li.insertBefore(p, removeBtn);
 			li.removeChild(editInput);
-			
-		}
+			li.removeChild(okBtn);
+			li.appendChild(editBtn);
+		})
 
 	})
 }
