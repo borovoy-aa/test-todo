a6630518afe3393c1d773e785d0ad9e8486ee569
Unknown <work.borovoy@gmail.com>
2018-08-05 22:17:40 +0300

    add date

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const btnTask = document.getElementById('add-task');
 const newTask = document.getElementById('new-task');
 const mainWrap = document.getElementById('main');
 const listItem = document.getElementsByClassName('list');
+const inputDate = document.getElementById('date');
 
 const clear = () => {
 	newTask.value = "";
 }
 
-const nTask = () => {
+const nTask = (e) => {
 	const taskValue = newTask.value;
 	
 	const li = document.createElement('li');
 	li.classList.add('list')
 	
-
 	const removeBtn = document.createElement('button');
 	removeBtn.classList.add('removeBtn');
 	removeBtn.innerText = 'Remove Task';
@@ -26,47 +26,71 @@ const nTask = () => {
 	okBtn.classList.add('okBtn');
 	okBtn.innerText = 'Okay';
 
+	const checkBtn = document.createElement('input')
+	checkBtn.type = "checkbox";
+	checkBtn.classList.add('checkBtn');
+
 	var p = document.createElement('p');
 	p.classList.add('list-p');
 	li.appendChild(p);
 
+	if (inputDate.value !== '') {
+		var dateP = document.createElement('p');
+		dateP.classList.add('date-p');
+		dateP.innerText = inputDate.value
+		li.insertBefore(dateP, li.children[0]);
+	}
+	
 	p.innerHTML = taskValue;
 	li.appendChild(removeBtn);
 	li.appendChild(editBtn);
+	li.insertBefore(checkBtn, li.children[0]);
+
+	checkBtn.addEventListener( 'change', function() {
+    if(this.checked) {
+			p.style.textDecoration = "line-through";
+			p.style.color = "green"
+    } else {
+			p.style.textDecoration = "none";
+			p.style.color = "black"
+    }
+	});
 	
 	if (taskValue.trim()) {
 		mainWrap.appendChild(li);
 	}
-
+	
 	clear();
-
+	
 	removeBtn.addEventListener('click', function(e) {
 		mainWrap.removeChild(li);
 	});
 
-	var editInput = document.createElement('input');
+	const editP = function (e) {
+		console.log(this)
+		p.innerText = editInput.value;
+		li.insertBefore(p, removeBtn);
+		editInput.parentNode.removeChild(editInput);
+		this.parentNode.removeChild(this);
+		li.appendChild(editBtn);
+	}
 	
-	editBtn.addEventListener('click', function() {
-		
-		var pVal = p.textContent;
-		var eInputVal = editInput.value = pVal;
+	const editInput = document.createElement('input');
+	editInput.classList.add('editInput');
 
+	editBtn.addEventListener('click', function() {
 		li.removeChild(p);
 		li.insertBefore(editInput, removeBtn);
-		editInput.autofocus = true;
+		editInput.value = p.textContent;
+		editInput.focus();
 		li.removeChild(editBtn);
 		li.appendChild(okBtn);
+		okBtn.addEventListener('click', editP, false);
+	}, false)
 
-		okBtn.addEventListener('click', function() {
-			p.innerText = editInput.value;
-			li.insertBefore(p, removeBtn);
-			li.removeChild(editInput);
-			li.removeChild(okBtn);
-			li.appendChild(editBtn);
-		})
-
-	})
+	okBtn.removeEventListener('click', editP, false);
+	e.preventDefault();
 }
 
-
 btnTask.addEventListener('click', nTask, false);
+
